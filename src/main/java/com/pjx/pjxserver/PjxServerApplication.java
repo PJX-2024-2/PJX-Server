af33f85dcb8db9405abbe8a902c034261852d9f5
jamjamyy <cuy666@naver.com>
2024-09-25 21:29:28 +0900

    초기세팅

---
+++ src/main/java/com/pjx/pjxserver/PjxServerApplication.java
@@ -0,0 +1,13 @@
+package com.pjx.pjxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjxServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjx/pjxserver/PjxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjx.pjxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
