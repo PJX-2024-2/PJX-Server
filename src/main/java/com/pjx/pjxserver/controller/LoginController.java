c96b2721d5949c5cff606ed57fc5ab291d07277f
jamjamyy <jamjamyy@kau.kr>
2024-10-17 17:22:36 +0900

    kakao)

--- src/main/java/com/pjx/pjxserver/controller/LoginController.java
+++ src/main/java/com/pjx/pjxserver/controller/LoginController.java
@@ -1,13 +1,28 @@
 package com.pjx.pjxserver.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
     @GetMapping({"/", ""})
     public String login() {
-        return "login";
+        return "Please login using Kakao";
+    }
+
+    @GetMapping("/loginSuccess")
+    public String loginSuccess(@AuthenticationPrincipal OAuth2User oauth2User) {
+        Map<String, Object> attributes = oauth2User.getAttributes();
+        // 카카오톡에서 제공하는 닉네임을 가져옴
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        String nickname = (String) profile.get("nickname");
+
+        return "Hello, " + nickname + "!";
     }
 }
