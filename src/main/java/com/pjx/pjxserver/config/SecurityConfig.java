19e3a3abc561274d5f465525e0abd58de3044dae
jamjamyy <139212312+jamjamyy@users.noreply.github.com>
2024-10-31 17:13:18 +0900

    Update SecurityConfig.java
    CORS

--- src/main/java/com/pjx/pjxserver/config/SecurityConfig.java
+++ src/main/java/com/pjx/pjxserver/config/SecurityConfig.java
@@ -15,11 +15,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(withDefaults()) // Enable CORS
-                .csrf(csrf -> csrf.disable()) // Globally disable CSRF
+                .cors(withDefaults()) 
+                .csrf(csrf -> csrf.disable()) 
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(  "/api/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated() // Require authentication for all other requests
+                        .requestMatchers("/api/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated() 
                 );
 
         return http.build();
@@ -31,9 +31,16 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*") // Allow all origins; specify your frontend URL here in production
+                        .allowedOrigins(
+                            "http://localhost:5173", 
+                            "https://pjx-client-4bsx.vercel.app",
+                            "http://localhost:8080", 
+                            "http://localhost:443",
+                            "http://localhost:80"
+                        ) 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true); 
             }
         };
     }
