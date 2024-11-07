792cfbffe299ad589f00586b1f8081d8934cc0bd
jamjamyy <139212312+jamjamyy@users.noreply.github.com>
2024-11-08 00:32:12 +0900

    Create Friend.java

---
+++ src/main/java/com/pjx/pjxserver/domain/Friend.java
@@ -0,0 +1,32 @@
+package com.pjx.pjxserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Friend {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "friend_id", nullable = false)
+    private User friend;
+
+    public Friend(User user, User friend) {
+        this.user = user;
+        this.friend = friend;
+    }
+}
