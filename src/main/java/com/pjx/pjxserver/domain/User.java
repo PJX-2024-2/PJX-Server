f27923edb09da2b1e3a9abe461c33333179c314b
jamjamyy <139212312+jamjamyy@users.noreply.github.com>
2024-11-21 04:10:20 +0900

    Update User.java

--- src/main/java/com/pjx/pjxserver/domain/User.java
+++ src/main/java/com/pjx/pjxserver/domain/User.java
@@ -17,10 +17,13 @@ public class User {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private Long kakaoId; 
+    private Long kakaoId;
 
     @Column(unique = true, nullable = false)
-    private String nickname; 
+    private String nickname;// 카카오에서 제공하는 닉네임
+
+    @Column
+    private String userNickname; // 애플리케이션 닉네임
 
     private String profileImageUrl;
 }
