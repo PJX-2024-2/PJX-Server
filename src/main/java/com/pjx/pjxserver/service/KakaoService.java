98cac9bcdf957c1c18e34aa69a170e00d332c6b1
jamjamyy <139212312+jamjamyy@users.noreply.github.com>
2024-11-24 02:02:40 +0900

    Update KakaoService.java

--- src/main/java/com/pjx/pjxserver/service/KakaoService.java
+++ src/main/java/com/pjx/pjxserver/service/KakaoService.java
@@ -21,18 +21,27 @@ public class KakaoService {
     @Value("${kakao.client_secret}")
     private String clientSecret;
 
-    @Value("${kakao.redirect_uri}")
-    private String redirectUri;
+    @Value("${kakao.redirect_uri.local}")
+    private String localRedirectUri;
+
+    @Value("${kakao.redirect_uri.prod}")
+    private String prodRedirectUri;
 
     private final WebClient.Builder webClientBuilder;
 
-    public Mono<KakaoTokenResponseDto> getAccessToken(String code) {
+
+    public Mono<KakaoTokenResponseDto> getAccessToken(String code, String origin) {
+        String redirectUri = determineRedirectUri(origin);
+        log.info("Using redirect_uri: {}", redirectUri);
+
         return webClientBuilder.build()
                 .post()
                 .uri("https://kauth.kakao.com/oauth/token")
                 .header(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
-                .bodyValue("grant_type=authorization_code&client_id=" + clientId + "&redirect_uri=" + redirectUri +
-                        "&code=" + code + "&client_secret=" + clientSecret)
+                .bodyValue("grant_type=authorization_code&client_id=" + clientId +
+                        "&redirect_uri=" + redirectUri +
+                        "&code=" + code +
+                        "&client_secret=" + clientSecret)
                 .retrieve()
                 .bodyToMono(KakaoTokenResponseDto.class);
     }
@@ -44,5 +53,18 @@ public class KakaoService {
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
                 .retrieve()
                 .bodyToMono(KakaoUserInfoResponseDto.class);
+    }
+
+    private String determineRedirectUri(String origin) {
+        if (origin == null) {
+            log.warn("Origin header is missing. Defaulting to prod redirect_uri.");
+            return prodRedirectUri; // 기본값으로 배포 환경 URI 사용
+        }
+
+        if (origin.contains("localhost")) {
+            return localRedirectUri; // 로컬 환경
+        }
+
+        return prodRedirectUri; // 배포 환경
     }
 }
